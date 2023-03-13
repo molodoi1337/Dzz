fc6d4cb7f4c34be4a499338c36d2397689a397a1
molodoi1337 <nniki@DESKTOP-8AHT603>
2023-03-13 20:59:59 +0300

    Добавьте файлы проекта.

---
+++ dz/dz.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+ using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+   
+
+    string s;
+    ifstream in("D:\\file.txt");
+
+    map<string, int> word;
+
+    while (in >> s)
+        word[s]++;
+    in.close();
+
+    map <string, int>::iterator it;
+    ofstream res("result.txt");
+    for (it = word.begin(); it != word.end(); it++)
+        res << it->first << ": " << it->second << endl;
+    return 0;
+    //Просто нереальная скорость выполнения программы
+}
